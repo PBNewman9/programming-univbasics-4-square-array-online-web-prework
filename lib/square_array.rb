038cedb9fb1d07924f2266c4bb5b1f87ea437cb2
Phil Newman <pnewman@viimed.com>
2019-10-28 13:47:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,8 @@
-
-
-
-
 def output_array_elements (square_array)
   square_array = [1,2,3]
   counter = 0 
   
   while square_array[counter] do
-    puts = square_array([1,2,3])**2
+    puts square_array**2
   counter +=1 
 end
